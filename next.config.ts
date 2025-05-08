c886e61b53e9834a773fdff42c2b86a461945d2b
Wael Louati <waelwzwz@gmail.com>
2025-05-08 13:35:21 +0200

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"]
 };
 
 export default nextConfig;
