93e7cf138807833b7856c0153c183792caeb097a
Wael Louati <waelwzwz@gmail.com>
2025-06-30 16:16:14 +0200

    chore: update Jest configuration to enable coverage reporting and remove unused sample JSON file

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,14 @@ require("dotenv/config");
 
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node", // changed to jsdom for React Testing Library
+  testEnvironment: "node", // or "jsdom" if you use React Testing Library
   testMatch: ["**/__tests__/**/*.test.ts", "**/__tests__/**/*.test.tsx"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
   rootDir: ".",
-  setupFilesAfterEnv: ["<rootDir>/src/__tests__/jest.setup.ts"], // <--- add this line
+  setupFilesAfterEnv: ["<rootDir>/src/__tests__/jest.setup.ts"],
+  collectCoverage: true, // <--- add this line
+  coverageReporters: ["lcov", "text"], // <--- add this line
+  coverageDirectory: "coverage", // <--- optional, default is "coverage"
 };
