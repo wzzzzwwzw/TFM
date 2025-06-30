88d4aeed30192f1035966833b396f932b354fdef
Wael Louati <waelwzwz@gmail.com>
2025-06-30 20:39:10 +0200

    fix: correct application title in metadata from "QuizUPMw" to "QuizUPM"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "QuizUPMw",
+  title: "QuizUPM",
   description: "Quiz yourself!",
 };
 
