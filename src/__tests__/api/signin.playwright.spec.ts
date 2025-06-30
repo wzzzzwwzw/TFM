025df1cfc3d7fab7e09f95bbaf6f0d5111c34ca8
Wael Louati <waelwzwz@gmail.com>
2025-06-30 13:54:10 +0200

    Remove __tests__ folder from repository

--- src/__tests__/api/signin.playwright.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('user can see Google Sign In with Google button', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-  const signInButton = page.getByRole('button', { name: /sign in with google/i });
-  await expect(signInButton).toBeVisible();
-});
